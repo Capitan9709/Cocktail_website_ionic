5cd7831fbba9eb8f218a97a11476b4d627bf0131
PTMARKETING01 <ptmarketing01@MEGACENTRAL.local>
2023-04-10 18:29:24 +0200

    segunda subida

--- src/views/Cocktails.vue
+++ src/views/Cocktails.vue
@@ -18,14 +18,50 @@
 
       <div id="container">
         <strong class="capitalize">Cocktails</strong>
-        <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <ion-item>
+          <ion-input aria-label="filter" placeholder="Write Something..." v-model="filter" @keyup.enter="callApi"></ion-input>
+        </ion-item>
+        <ion-button @click="callApi">Buscar</ion-button>
+
+        <div id="cocktailsContainer">
+          <ion-content v-if="data.drinks">
+            <ion-card v-for="cocktail in data.drinks" :key="cocktail.idDrink">
+              <ion-card-header>
+                <ion-card-title>{{ cocktail.strDrink }}</ion-card-title>
+              </ion-card-header>
+              <ion-card-content>
+                <p>{{ cocktail.strAlcoholic }}</p>
+              </ion-card-content>
+            </ion-card>
+          </ion-content>
+        </div>
       </div>
     </ion-content>
   </ion-page>
 </template>
   
 <script setup lang="ts">
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonLabel, IonItem, IonInput, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { ref } from 'vue';
+
+const filter = ref('');
+const inputText = ref('');
+var data = "";
+
+function printFilter(){
+  inputText.value = filter.value;
+}
+
+async function callApi(){
+  try{
+    inputText.value = filter.value;
+    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputText.value}`);
+    data = await response.json();
+    console.log(data.drinks);
+  }catch(error){
+    console.log(error);
+  }
+}
 
 </script>
   
@@ -35,7 +71,7 @@ import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, Io
   position: absolute;
   left: 0;
   right: 0;
-  top: 50%;
+  top: 25%;
   transform: translateY(-50%);
 }
 
